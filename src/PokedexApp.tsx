6d1c5cd644def6fdbd57cc2330abf0619fa4f6f9
Manuel Cornejo <manucornejo@Manu-Cornejo-2.local>
2024-05-14 12:45:25 -0400

    feat: se agrega react native paper al proyecto

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './presentation/navigator/StackNavigator';
+import {PaperProvider} from 'react-native-paper';
 
 const PokedexApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {Button} from 'react-native-paper';
 
 export default class HomeScreen extends Component {
   render() {
     return (
       <View>
         <Text> HomeScreen </Text>
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
       </View>
     );
   }
